fe5966a6187edba25023991bee168abd5e56046e
Josiel <medeiros.josiel@gmail.com>
2026-04-08 16:55:16 -0300

    feat: initialize project with TanStack Start, Prisma, Tailwind CSS, and Biome configuration

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({ component: App });
+
+function App() {
+	return (
+		<main className="page-wrap px-4 pb-8 pt-14">
+			<section>
+				<h1 className="display-title mb-5 max-w-3xl text-4xl leading-[1.02] font-bold tracking-tight  sm:text-6xl">
+					Welcome back
+				</h1>
+			</section>
+		</main>
+	);
+}
